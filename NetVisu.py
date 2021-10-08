aa101153d8abf14a59e510da0fc375dd881eae79
H Ennes <hlovisiennes@gmail.com>
2021-10-08 14:09:55 -0300

    Update NetVisu.py

--- NetVisu.py
+++ NetVisu.py
@@ -65,53 +65,49 @@ def trains_network(model, number_of_epochs, data, target, verbose = 2, plot = Fa
     return 
 
 #defines the function that plots the PCAed output of each layer
-def pca_network(number_of_layers, neurons_per_hidden_layer, activation, data, target,
-                inputs_shape = 3, number_of_epochs = 100, test_size = 0.15):
+def pca_network(model, data, target):
     '''
     Plots the pca output of each hidden layer to 3 dimensions (if the dimensions are bigger than 3) for a neural network model
-    Input: number_of_layers: number of hidden layers in the network; neurons_per_hidden_layer: width of each hidden layer, which must be the
-    same for all layers; activation: activation function for each hidden layer, which must be the same; data: dataset; target: dataset's 
-    labels; inputs_shape = 3: shape of input set to 3 by default number_of_epochs=100: number of training epochs, set by default to 100;
-    test_size = 0.15: selects percentage of dataset to be used as validation data, set to 0.15
+    Input: nmodel: Feedforward Neural Network model; data: dataset; target: dataset's labels
     '''
-    if neurons_per_hidden_layer>3:
-        model = feedforward(number_of_layers, neurons_per_hidden_layer, activation, inputs_shape = inputs_shape)
-        trains_network(model, number_of_epochs, data, target, verbose = 0)
-        pca = PCA(n_components=3)
+    model_copy = keras.models.clone_model(model)
+    model_copy.set_weights(model.get_weights()) 
+    model_copy.compile(loss='binary_crossentropy', optimizer='Adam', metrics=['accuracy'])
+    model_copy.evaluate(data, target)
+    
+    pca = PCA(n_components=3)
 
-        for i in range(number_of_layers):
-            layer = tf.keras.models.Model(inputs=model.input,outputs=model.layers[i].output)
-            layer_output = layer.predict(data)
-            pca_data= pca.fit_transform(layer_output)
-            fig = plt.figure()
-            ax = Axes3D(fig)
+    for i in range(len(model_copy.layers)-1):
+        layer = tf.keras.models.Model(inputs=model_copy.input,outputs=model_copy.layers[i].output)
+        layer_output = layer.predict(data)
+        pca_data= pca.fit_transform(layer_output)
+        fig = plt.figure()
+        ax = Axes3D(fig)
 
-            red = pca_data[target == 0]
-            blue = pca_data[target == 1]
-            scatter = ax.scatter(red[:,0], red[:,1], red[:,2],c='r')
-            scatter = ax.scatter(blue[:,0], blue[:,1], blue[:,2],c='b')
-            plt.show()
-    else:
-        raise(Exception("Layers width too narrow!"))
+        red = pca_data[target == 0]
+        blue = pca_data[target == 1]
+        scatter = ax.scatter(red[:,0], red[:,1], red[:,2],c='r')
+        scatter = ax.scatter(blue[:,0], blue[:,1], blue[:,2],c='b')
+        ax.text2D(0.05, 0.95, "Hidden Layer "+str(i+1), transform=ax.transAxes)
+        plt.show()
 
 #define function that plots persistence homology study of each layer output
-def persis_homol_network(number_of_layers, neurons_per_hidden_layer, activation, data, target, intrinsic_dimension = 2, pers_dis = 1,
-                inputs_shape = 3, number_of_epochs = 100, test_size = 0.15):
+
+def persis_homol_network(model, data, target, intrinsic_dimension = 2, pers_dis = 1):
     '''
     Plots the persitence homology barcodes and diagrams of each layer of the neural network
-    Input:  number_of_layers: number of hidden layers in the network; neurons_per_hidden_layer: width of each hidden layer, which must be
-    the same for all layers; activation: activation function for each hidden layer, which must be the same; data: dataset; target: dataset's 
+    Input:  model: Feedforward Neural Network model; data: dataset; target: dataset's 
     labels; intrinsic_dimension = 1: intrinsic manifold dimension, so we know up to where perform the persistence homology;
-    per_dis = 1: maximum distance to calculate persistence homology,
-    inputs_shape = 3: shape of input set to 3 by default number_of_epochs=100: number of training epochs, set by default to 100;
-    test_size = 0.15: selects percentage of dataset to be used as validation data, set to 0.15 
+    per_dis = 1: maximum distance to calculate persistence homology, 
     '''
-    model = feedforward(number_of_layers, neurons_per_hidden_layer, activation, inputs_shape = inputs_shape)
-    trains_network(model, number_of_epochs, data, target, verbose = 0)
-    pca = PCA(n_components=3)
+    model_copy = keras.models.clone_model(model)
+    model_copy.set_weights(model.get_weights()) 
+    model_copy.compile(loss='binary_crossentropy', optimizer='Adam', metrics=['accuracy'])
+    model_copy.evaluate(data, target)
     
-    for i in range(number_of_layers):
-        layer = tf.keras.models.Model(inputs=model.input,outputs=model.layers[i].output)
+    
+    for i in range(len(model_copy.layers)-1):
+        layer = tf.keras.models.Model(inputs=model_copy.input,outputs=model_copy.layers[i].output)
         layer_output = layer.predict(data)
         rips = gudhi.RipsComplex(points = layer_output, max_edge_length = pers_dis)
         st = rips.create_simplex_tree(max_dimension = intrinsic_dimension)
@@ -121,42 +117,6 @@ def persis_homol_network(number_of_layers, neurons_per_hidden_layer, activation,
         ax1 = fig.add_subplot(1,2,1); ax2 = fig.add_subplot(1,2,2)
         gudhi.plot_persistence_barcode(barcodes, axes = ax1)
         gudhi.plot_persistence_diagram(barcodes, axes = ax2)
+        fig.suptitle('Hidden Layer'+str(i+1))
         plt.show()
-        
-def pca_and_persistence(number_of_layers, neurons_per_hidden_layer, activation, data, target, intrinsic_dimension = 2, pers_dis = 1,
-                inputs_shape = 3, number_of_epochs = 100, test_size = 0.15):
-    '''
-    Plots both the pca and the persitence homology barcodes and diagrams of each layer of the neural network, good for avoiding the
-    stochastic effect of the neural network model
-    Input:  number_of_layers: number of hidden layers in the network; neurons_per_hidden_layer: width of each hidden layer, which must be
-    the same for all layers; activation: activation function for each hidden layer, which must be the same; data: dataset; target: dataset's 
-    labels; intrinsic_dimension = 1: intrinsic manifold dimension, so we know up to where perform the persistence homology;
-    per_dis = 1: maximum distance to calculate persistence homology,
-    inputs_shape = 3: shape of input set to 3 by default number_of_epochs=100: number of training epochs, set by default to 100;
-    test_size = 0.15: selects percentage of dataset to be used as validation data, set to 0.15 
-    '''
-    if neurons_per_hidden_layer>3:
-        model = feedforward(number_of_layers, neurons_per_hidden_layer, activation, inputs_shape = inputs_shape)
-        trains_network(model, number_of_epochs, data, target, verbose = 0)
-        pca = PCA(n_components=3)
-        for i in range(number_of_layers):
-            layer = tf.keras.models.Model(inputs=model.input,outputs=model.layers[i].output)
-            layer_output = layer.predict(data)
-            pca_data= pca.fit_transform(layer_output)
-            fig = plt.figure()
-            ax = Axes3D(fig)
-
-            red = pca_data[target == 0]
-            blue = pca_data[target == 1]
-            scatter = ax.scatter(red[:,0], red[:,1], red[:,2],c='r')
-            scatter = ax.scatter(blue[:,0], blue[:,1], blue[:,2],c='b')
-            rips = gudhi.RipsComplex(points = layer_output, max_edge_length = 1)
-            st = rips.create_simplex_tree(max_dimension = 2)
-
-            barcodes = st.persistence(homology_coeff_field = 2)
-            fig = plt.figure(figsize=(15,5))
-            ax1 = fig.add_subplot(1,2,1); ax2 = fig.add_subplot(1,2,2)
-            gudhi.plot_persistence_barcode(barcodes, axes = ax1)
-            gudhi.plot_persistence_diagram(barcodes, axes = ax2)
-            plt.show()
     
